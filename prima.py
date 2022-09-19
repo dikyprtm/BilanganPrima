c274358a35b41c364421ede0f2ab3196cb3efa83
LIezi <43513141+dikyprtm@users.noreply.github.com>
2022-09-19 11:59:58 +0700

    Create prima.py

---
+++ prima.py
@@ -0,0 +1,16 @@
+angka = 2
+
+while angka <=2:
+    angka = int(input("Input sebuah bilangan: "))
+
+    if angka <=2:
+        print("Input salah. Ulangi lagi.")
+
+    if angka >=2:
+        for i in range (2, angka):
+            if (angka % i) == 0:
+                print(angka, "BUKAN bilangan prima")
+                break
+        else:
+            print(angka, "adalah bilangan prima")
+            break
